0348133fd7a9d11f788fdd4888b36744ba2f4cac
Andreea Filipciuc <Andreea.Filipciuc@catalysts.cc>
2018-09-13 17:09:18 +0300

    login and logout

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,24 +1,25 @@
-import Vue from 'vue'
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import VueApollo from 'vue-apollo'
+import Vue from 'vue';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
 
 const httpLink = new HttpLink({
     // You should use an absolute URL here
     uri: 'https://dvkiin.xyz/graphql',
-})
+    // uri: 'http://localhost:8000/graphql',
+});
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
     link: httpLink,
     cache: new InMemoryCache(),
     connectToDevTools: true,
-})
+});
 
 export default new VueApollo({
     defaultClient: apolloClient,
-})
+});
 
 // Install the vue plugin
-Vue.use(VueApollo)
+Vue.use(VueApollo);
