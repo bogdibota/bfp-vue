34bb0bd9ed0f15ea8cce9807dba84802b092d94f
Andreea Onaciu <andreeaonaciu@yahoo.com>
2018-11-22 13:36:08 +0200

    group information section - group owner (#26)

---
+++ src/components/GroupInformation.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="groupInfo">
+        <h3>Group owner</h3>
+        <UserCard :user="groupOwner"/>
+    </div>
+</template>
+
+<script>
+    import UserCard from './UserCard';
+
+    export default {
+        name: 'group-information',
+        props: ['groupOwner'],
+        components: {
+            UserCard,
+        },
+    };
+</script>
+
+<style scoped>
+    .groupInfo {
+        width: 240px;
+        margin: 30px;
+    }
+</style>
